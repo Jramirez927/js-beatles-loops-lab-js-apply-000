050d72e93f5da22eafe6e5bdbb9b2150ee167cfc
Jorge Ramirez <jorgeramirez927@gmail.com>
2018-03-02 00:12:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 function theBeatlesPlay(musicians, instruments){
-  for( i = 0; i < musicians.lenghth; i++;){
-    
+  results = [];
+  for( i = 0; i < musicians.lenghth; i++){
+    results.push(musicians[i] +" plays "+ instruments[i])
   }
+}
+
+function johnLennonFacts(facts){
+  while( facts.pop())
 }
